9bdb2fe1afb2bfcbb60e8bac202a3b4072d5d913
VictorMerinoITCBCN <2023_victor.merino@iticbcn.cat>
2025-05-07 15:53:04 +0200

    added swagger authorization

---
+++ api/src/main/java/com/pauseapp/api/swagger/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package com.pauseapp.api.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+            .info(new Info()
+                .title("PauseApp API")
+                .version("1.0")
+                .description("Documentación de la API protegida con JWT"))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
